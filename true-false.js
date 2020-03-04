08a3630673a1d0f6ef38cda03cd92b4b1c5d0895
arcpy-naf <arcpy.naf@gmail.com>
2020-03-04 16:31:20 +0600

    truthy-falsy

---
+++ true-false.js
@@ -0,0 +1,16 @@
+//const name = "" ;
+/*false 
+""
+undefined
+null
+NaN
+*/
+
+let name = null;
+console.log(name)
+if(name){
+    console.log("the condition is true")
+}
+else{
+    console.log("the condition is false")
+}
